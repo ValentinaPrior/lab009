f32be849944b20601abc1d8b76d2c39f27e5072a
ValentinaPrior <vbp7@njit.edu>
2024-11-22 08:40:58 -0500

    q2

--- style.js
+++ style.js
@@ -6,3 +6,8 @@ for (i=0; i<=4; i++)
     let answer= characters[i];
     console.log(answer);
 }
+
+//QUESTION TWO
+let firstName='Harry';
+let house='Gryffindor';
+console.log (`Welcome, ${firstName} of ${house} !`);
