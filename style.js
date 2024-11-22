dece447c11e39db3b6a8846318c319107301f6a5
ValentinaPrior <vbp7@njit.edu>
2024-11-22 13:04:12 -0500

    q14

--- style.js
+++ style.js
@@ -66,3 +66,8 @@ console.log(answer);
 let wizards=['Harry','Hermione','Ron'];
 wizards.push('Luna','Draco');
 console.log(wizards);
+
+//QUESTION FOURTEEN
+let message=['Welcome to Hogwarts School of Witchcraft and Wizardry'];
+let newMessage=message.slice(11,19).concat('castle');
+console.log(newMessage);
